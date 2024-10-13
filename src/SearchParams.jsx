b2757aeee88d098298e482e8084f1dbf30e4a56f
nikiiigoyal <nikiiigoyal@gmail.com>
2024-10-13 21:54:36 +0530

    complete project

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,54 +1,72 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import Results from "./Results";
+import AdoptedPetContext from "./AdoptedPetContext";
+import useBreedList from "./useBreedList";
+import fetchSearch from "./fetchSearch";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
-
-
 const SearchParams = () => {
-const {location, setLocation} = useState("");
-const [animal, setAnimal] = useState("");
-const [breed, setBreed] = useState("");
-const breeds = []
+  const [requestParams, setRequestParams] = useState({
+    location: "",
+    animal: "",
+    breed: "",
+  });
+  const [adoptedPet] = useContext(AdoptedPetContext);
+  const [animal, setAnimal] = useState("");
+  const [breeds] = useBreedList(animal);
+
+  const results = useQuery(["search", requestParams], fetchSearch);
+  const pets = results?.data?.pets ?? [];
+
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.target);
+          const obj = {
+            animal: formData.get("animal") ?? "",
+            breed: formData.get("breed") ?? "",
+            location: formData.get("location") ?? "",
+          };
+          setRequestParams(obj);
+        }}
+      >
+        {adoptedPet ? (
+          <div className="pet image-container">
+            <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+          </div>
+        ) : null}
         <label htmlFor="location">
           Location
-          <input onChange= {(e) => setLocation(e.target.value)}
-          id="location" 
-          value={location} placeholder="Location" />
+          <input id="location" name="location" placeholder="Location" />
         </label>
-        <label htmlFor="animal">Animal
-            <select
-        id="animal"
-    value={animal}
-    onChange={(e) => {
-      setAnimal(e.target.value);
-      setBreed("");
-    }}
-    onBlur={(e) => {
-      setAnimal(e.target.value);
-      setBreed("");
-    }}
-  >
-    <option />
 
-    {ANIMALS.map((animal) => (
-      <option key={animal} value={animal}>
-        {animal}
-      </option>
-    ))}
-            </select>
-
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            name="animal"
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
+
         <label htmlFor="breed">
           Breed
-          <select
-            disabled={!breeds.length}
-            id="breed"
-            value={breed}
-            onChange={(e) => setBreed(e.target.value)}
-            onBlur={(e) => setBreed(e.target.value)}
-          >
+          <select disabled={!breeds.length} id="breed" name="breed">
             <option />
             {breeds.map((breed) => (
               <option key={breed} value={breed}>
@@ -57,11 +75,12 @@ const breeds = []
             ))}
           </select>
         </label>
+
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
 
 export default SearchParams;
-
--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,39 @@
-
 import { createRoot } from "react-dom/client";
-// import Pet from "./Pet";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "./Details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
+// import useState and AdoptedPetContext
+import { useState } from "react";
+import AdoptedPetContext from "./AdoptedPetContext";
 
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
+  const adoptedPet = useState(null);
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams/>
+      <BrowserRouter>
+       <QueryClientProvider client={queryClient}>
+      <AdoptedPetContext.Provider value={adoptedPet}>
+         <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+        </AdoptedPetContext.Provider>
+        </QueryClientProvider>
+        </BrowserRouter>
     </div>
   );
 };
